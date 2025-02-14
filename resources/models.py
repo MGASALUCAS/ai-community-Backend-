8d8c39b951301e52beba84994c15a5f66bf28138
Computer Wizard <mgasa.loucat1@gmail.com>
2025-02-14 10:11:59 +0300

    added resource add, fetch, delete API endpoints

--- resources/models.py
+++ resources/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Resource(models.Model):
+    CATEGORY_CHOICES = [
+        ('technical', 'Technical'),
+        ('non-technical', 'Non-Technical'),
+        ('data', 'Data'),
+        ('infrastructure', 'Infrastructure'),
+        ('reports', 'Reports'),
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='resources_images/')
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    link = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- resources/views.py
+++ resources/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Resource
+from .serializers import ResourceSerializer
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+# List all resources or create new one
+class ResourceListCreateView(generics.ListCreateAPIView):
+    queryset = Resource.objects.all()
+    serializer_class = ResourceSerializer
+    permission_classes = [AllowAny]
+
+# Retrieve, update, or delete a specific resource
+class ResourceDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Resource.objects.all()
+    serializer_class = ResourceSerializer
+    permission_classes = [AllowAny]
