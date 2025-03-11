87b3c0393071cbe1ef9406363d9a568a0c4f18b0
Computer Wizard <mgasa.loucat1@gmail.com>
2025-03-11 15:45:43 +0300

    resolving got commit issue

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1, localhost").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost,ai-community-backend-29pmq.ondigitalocean.app").split(",")
 
 
 # Application definition
