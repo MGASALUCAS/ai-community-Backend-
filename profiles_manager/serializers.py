918e3542495bb2c802d18965256ac9ddc90af4c3
Computer Wizard <mgasa.loucat1@gmail.com>
2025-03-14 15:17:04 +0300

    changes in email field

--- profiles_manager/serializers.py
+++ profiles_manager/serializers.py
@@ -1,43 +1,51 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from django.contrib.auth.password_validation import validate_password
 from rest_framework_simplejwt.tokens import RefreshToken
 from allauth.socialaccount.models import SocialAccount
 
 User = get_user_model()
 
-class SignupSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, validators=[validate_password])
-    confirm_password = serializers.CharField(write_only=True)
-    
+class ProfileSerializer(serializers.ModelSerializer):
+    whatsappNumber = serializers.CharField(source='profile.whatsapp_number', required=False)
+    twitter = serializers.URLField(source='profile.twitter', required=False)
+    github = serializers.URLField(source='profile.github', required=False)
+    linkedIn = serializers.URLField(source='profile.linkedin', required=False)
+    instagram = serializers.URLField(source='profile.instagram', required=False)
+    sectors = serializers.ListField(child=serializers.CharField(), source='profile.sectors', required=False)
+    technologies = serializers.ListField(child=serializers.CharField(), source='profile.technologies', required=False)
+    education = serializers.JSONField(source='profile.education', required=False)
+    achievements = serializers.JSONField(source='profile.achievements', required=False)
+    projects = serializers.JSONField(source='profile.projects', required=False)
+
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password', 'confirm_password', 'gender')
-    
-    def validate(self, data):
-        if data['password'] != data['confirm_password']:
-            raise serializers.ValidationError("Passwords do not match")
-        return data
-    
+        fields = ['id', 'username', 'email', 'whatsappNumber', 'twitter', 'github', 'linkedIn', 'instagram', 'sectors', 'technologies', 'education', 'achievements', 'projects']
+
+class SignupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
     def create(self, validated_data):
-        validated_data.pop('confirm_password')
         user = User.objects.create_user(**validated_data)
         return user
 
 class SigninSerializer(serializers.Serializer):
-    username = serializers.CharField()
+    email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
 
     def validate(self, data):
-        user = User.objects.filter(username=data['username']).first()
-        if user and user.check_password(data['password']):
+        user = authenticate(**data)
+        if user and user.is_active:
             return user
         raise serializers.ValidationError("Invalid credentials")
 
 class UpdateProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email', 'gender')
+        fields = ['username', 'email', 'profile']
 
 class ForgotPasswordSerializer(serializers.Serializer):
     email = serializers.EmailField()
